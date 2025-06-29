a30dfcddf8a670ea99a4d3a04a0b937f8fd9da61
MrVolfex <mihailo538@gmail.com>
2025-06-29 09:50:49 +0200

    first commit

---
+++ src/app/(tabs)/index.tsx
@@ -0,0 +1,17 @@
+
+import {View} from 'react-native';
+
+import products from '@/assets/data/products';
+import ProductListItem from '@/src/components/ProductListItem';
+
+
+
+export default function ManuScreen() {
+  return (
+    <View>
+      <ProductListItem product={products[5]}/>
+      <ProductListItem product={products[1]}/>
+    </View>
+   
+  );
+}
